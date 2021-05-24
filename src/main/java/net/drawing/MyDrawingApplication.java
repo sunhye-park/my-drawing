400d99f7f8ce62d0a8f5481e4bdf2dee8ee6f147
DESKTOP-C93KU0P\ev91 <ev91@naver.com>
2021-05-24 15:49:46 +0900

    initial commit

---
+++ src/main/java/net/drawing/MyDrawingApplication.java
@@ -0,0 +1,13 @@
+package net.drawing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDrawingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDrawingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/drawing/MyDrawingApplicationTests.java
@@ -0,0 +1,13 @@
+package net.drawing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDrawingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
